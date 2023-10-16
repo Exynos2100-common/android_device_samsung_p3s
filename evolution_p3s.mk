21f65ddcd86916afaaae5185f4d41fce25f92ba4
TQMatvey <maybeetube@gmail.com>
2023-10-16 12:40:06 +0700

    p3s: Evolution-X Bringup

--- evolution_p3s.mk
+++ evolution_p3s.mk
@@ -22,18 +22,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/p3s/device.mk)
 
 ## Boot Animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_HEIGHT := 3200
-TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common Evolution X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Evolution-X Flags
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_MINI_GAPPS := true
+
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_p3s
+PRODUCT_NAME := evolution_p3s
 PRODUCT_DEVICE := p3s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G998B
